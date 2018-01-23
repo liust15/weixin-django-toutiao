e6d5c31da465b35913c9b1a5c7417568af1993e7
MAC <mac@MACdeMacBook-Pro.local>
2018-01-23 20:36:37 +0800

    二次提交

--- toutiao/settings.py
+++ toutiao/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',  
         'NAME': 'toutiao',  
         'USER': 'root',  
-        'PASSWORD': '5515lsT@',  
+        'PASSWORD': 'root',  
         'HOST': '127.0.0.1',  
         'PORT': '3306',  
     } 
